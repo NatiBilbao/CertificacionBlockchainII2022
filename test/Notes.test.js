d45665d3e504cdebec467d4fa84f9a1a419f01a9
NatyBilbao <natabilbaocano18@gmail.com>
2022-06-15 00:15:10 -0400

    9/6/2022 Tarea 3 - Unit testing Evaluar() solo el profesor puede util…

---
+++ test/Notes.test.js
@@ -0,0 +1,24 @@
+const Notes = artifacts.require("Notes");
+
+contract("Notes", (accounts) => {
+    let instance;
+
+    beforeEach("deploys a contract", async () => {
+        instance = await Notes.new();
+    });
+
+    it("Evaluar", async () => {
+        await instance.Evaluar("Ricardo Pari", 5, { from: accounts[0] });
+        const id = web3.utils.keccak256("Ricardo Pari");
+        const note = await instance.Notas.call(id);
+        assert.equal(5, note);
+    });
+
+    it("Evaluar 2", async () => {
+        await instance.Evaluar("Ricardo Pari", 5, { from: accounts[0] });
+        const note = await instance.VerNotas.call("Ricardo Pari");
+        assert.equal(5, note);
+    });
+
+    it("just proffesor", async () => {});
+});
