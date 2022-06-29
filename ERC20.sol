a7ce5f45d7e0e4afedb6b1aeaa6fb3d358736b71
NatyBilbao <natabilbaocano18@gmail.com>
2022-06-29 17:33:08 -0400

    29/6/2022 internal functions mint, burn

--- ERC20.sol
+++ ERC20.sol
@@ -41,7 +41,7 @@ contract ERC20 is IERC20, IERC20Metadata {
 
     function transfer(address _to, uint256 _value) public override returns (bool success){
         address owner = msg.sender;
-        _transfer(owner, to, amount);
+        _transfer(owner, _to, _value);
         return true;
     }
 
@@ -60,6 +60,61 @@ contract ERC20 is IERC20, IERC20Metadata {
         _spendAllowance(_from, spender, _value);
         _transfer(_from, _to, _value);
         return true;
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        require(from != address(0), "ERC20: Do not transfer from the zero address.");
+        require(from != address(0), "ERC20: Do not transfer to the zero address.");
+
+        uint256 fromBalance = _balances[from];
+        require(fromBalance >= amount, "ERC20: Transfer amount exceeds balance.");
+
+        // Save in gas fees
+    unchecked {
+        _balances[from] = fromBalance - amount;
+    }
+
+        _balances[to] += amount;
+
+        emit Transfer(from, to, amount);
+
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "ERC20: Do not approve from the zero address.");
+        require(spender != address(0), "ERC20: Do not approve to the zero address.");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+
+    }
+
+    function _spendAllowance(address owner, address spender, uint256 amount)internal {
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance < type(uint256).max){
+            require(currentAllowance >= amount, "ERC20: Current allowance is not enough to spend the intended amount");
+        unchecked{
+            _approve(owner, spender, currentAllowance - amount);
+        }
+        }
+    }
+
+    function _mint(address account, uint256 amount) internal{
+        require(account != address(0), "ERC20: Mint from the zero address is not valid.");
+        _totalSupply += amount;
+        _balances[account] += amount;
+
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: Burn from the zero address is not valid.");
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "ERC20: Amount should be less or equal to actual account's balance");
+        _balances[account] -= amount;
+        _totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
     }
 
 }
